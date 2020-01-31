e4030cb7a5f45df425abb0494b1713378f05283e
jyoung67 <36745757+jyoung67@users.noreply.github.com>
2020-01-30 22:19:11 -0500

    Functional Test

---
+++ labs/lab03/bayes_disease.R
@@ -0,0 +1,7 @@
+bayes_disease <- function(runs) 
+{
+  df <- data.frame(c(0.91, 0.09), c(0.16, 0.84))
+  prior <- c(0.001, 0.999)
+  emits <- replicate(runs, sample(c(1,2), 1, replace = TRUE, prob= c(df[1,1], df[1,2])))
+  result <- run_bayes_sim(prior, df, emits)
+}
