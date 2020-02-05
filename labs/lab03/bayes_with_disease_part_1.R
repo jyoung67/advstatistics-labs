eadf68eae6e08487dddcdd5a3daeb34c3b8038de
jyoung67 <36745757+jyoung67@users.noreply.github.com>
2020-02-05 09:09:14 -0500

    Created figures for part 1

--- labs/lab03/bayes_with_disease_part_1.R
+++ labs/lab03/bayes_with_disease_part_1.R
@@ -1,7 +1,7 @@
-bayes_with_disease <- function(maxEmitLength, iterations) 
+bayes_with_disease_part_1 <- function(maxEmitLength, iterations) 
 {
   # Example Function Call
-  # 
+  # result <- bayes_with_disease_part_1(maxEmitLength=40, iterations=1000)
   
   meanPostValues <- vector(mode = "numeric", length = maxEmitLength)
   powerValues <- vector(mode = "numeric", length = maxEmitLength)
@@ -24,5 +24,10 @@ bayes_with_disease <- function(maxEmitLength, iterations)
   plot(1:length(powerValues), powerValues, main = "Patient with Disease", xlab = "num of tests", ylab = "power value", xlim=c(1, maxEmitLength),ylim = c(0,1))
   abline(h = 0.95)
   text(7, .98, "power = 0.95")
+  
+  write.csv(powerValues, row.names = F, file = "/Users/young/Documents/GitHub/advstatistics-labs/labs/lab03/power_values_part_1.csv")
+  write.csv(meanPostValues, row.names = F, file = "/Users/young/Documents/GitHub/advstatistics-labs/labs/lab03/mean_posterior_values_part_1.csv")
+  # datafrm <- read.csv("/Users/young/Documents/GitHub/advstatistics-labs/labs/lab03/power_values_part_1.csv"")
+  
   return(data.frame(meanPostValues, powerValues))
 }
---
+++ labs/lab03/bayes_without_disease_part_1.R
@@ -0,0 +1,33 @@
+bayes_without_disease_part_1 <- function(maxEmitLength, iterations) 
+{
+  # Example Function Call
+  # result <- bayes_without_disease_part_1(maxEmitLength=40, iterations=1000)
+  
+  meanPostValues <- vector(mode = "numeric", length = maxEmitLength)
+  powerValues <- vector(mode = "numeric", length = maxEmitLength)
+  df <- data.frame(c(0.91, 0.09), c(0.16, 0.84))
+  prior <- c(0.001, 0.999)
+  
+  for(i in 1:maxEmitLength)
+  {
+    currentRun <- vector(mode = "numeric", length = iterations)
+    for(j in 1:iterations)
+    {
+      emits <- replicate(i, sample(c(1,2), 1, replace = TRUE, prob= c(df[2,1], df[2,2])))
+      result <- run_bayes_sim(prior, df, emits)
+      currentRun[j] <- result[length(result[,2]), 2]
+    }
+    meanPostValues[i] <- mean(currentRun)
+    powerValues[i] <- length(currentRun[currentRun >= 0.9999])/iterations
+  }
+  
+  plot(1:length(powerValues), powerValues, main = "Patient without Disease", xlab = "num of tests", ylab = "power value", xlim=c(1, maxEmitLength),ylim = c(0,1))
+  abline(h = 0.95)
+  text(20, .9, "power = 0.95")
+  
+  write.csv(powerValues, row.names = F, file = "/Users/young/Documents/GitHub/advstatistics-labs/labs/lab03/without_disease_power_values_part_1.csv")
+  write.csv(meanPostValues, row.names = F, file = "/Users/young/Documents/GitHub/advstatistics-labs/labs/lab03/without_disease_mean_posterior_values_part_1.csv")
+  # datafrm <- read.csv("/Users/young/Documents/GitHub/advstatistics-labs/labs/lab03/without_disease_power_values_part_1.csv"")
+  
+  return(data.frame(meanPostValues, powerValues))
+}
