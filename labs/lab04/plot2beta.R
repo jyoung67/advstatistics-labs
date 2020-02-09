6888583ceb53d1d23823f4fdd1214e14af8a6b57
jyoung67 <36745757+jyoung67@users.noreply.github.com>
2020-02-09 10:55:13 -0500

    updates

--- labs/lab04/plot2beta.R
+++ labs/lab04/plot2beta.R
@@ -1,5 +1,9 @@
 plot2beta <- function(dbeta1, dbeta2, lpos = "topleft")
 {
+  # Example Commands
+  # plot2beta(c(1,1), c(6,6))
+  # plot2beta(c(1+1,1+1), c(6+1,6+1))
+  # plot2beta(c(1+400,1+400), c(6+400,6+400))
   s <- seq(0,1,0.001)
   ymax = (dbeta2[1])/(dbeta2[1]+dbeta2[2])
   plot(s, dbeta(s,dbeta1[1],dbeta1[2]), col="blue", ylim=c(0,dbeta(ymax, dbeta2[1], dbeta2[2])), cex=0.2, xlab = "probs" ,ylab = "dbeta(...)")
