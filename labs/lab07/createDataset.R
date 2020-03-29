91b61c5f4c7904fa1e3c961f157f765e440aece1
jyoung67 <36745757+jyoung67@users.noreply.github.com>
2020-03-29 12:34:06 -0400

    Update createDataset.R

--- labs/lab07/createDataset.R
+++ labs/lab07/createDataset.R
@@ -23,9 +23,9 @@ createDataset <- function()
   }
   
   # Test only
-  #pValues <- c(0.0001, 0.0004, 0.0019, 0.0095, 0.0201, 0.0278, 0.0298, 
-              # 0.0344, 0.0459, 0.3240, 0.4262, 0.5719, 0.6528, 0.7590, 1.000)
-  #numRows <- length(pValues)
+  # pValues <- c(0.0001, 0.0004, 0.0019, 0.0095, 0.0201, 0.0278, 0.0298, 
+  #             0.0344, 0.0459, 0.3240, 0.4262, 0.5719, 0.6528, 0.7590, 1.000)
+  # numRows <- length(pValues)
   
   threshold <- 0.05
   default_result <- sum(pValues <= threshold)
@@ -33,12 +33,13 @@ createDataset <- function()
   Bonferroni_result <- sum(pValues <= threshold/numRows)
  
   # equivalent to:  p.adjust(pValues, method = "BH")
-  adjustedPValues <- ((numRows*pValues)/rank(pValues))
-  BHFDR_result <- sum(adjustedPValues <= threshold)
+  sortedPValues <- sort(pValues)
+  vals <- sortedPValues <= (rank(sortedPValues)/length(sortedPValues)) * threshold
+  BHFDR_result <- ifelse(length(which(vals == TRUE)) == 0 , 0, max(which(vals == TRUE) ) )
 
   print(paste("Default threshold:  # of significant values:", default_result))
   print(paste("Bonferroni adjusted threshold:  # of significant values:", Bonferroni_result))
   print(paste("BH FDR adjusted threshold:  # of significant values:", BHFDR_result))
-  
-  return (data.frame(raw=pValues, adjusted=adjustedPValues))
+ 
+  return (pValues)
 }
