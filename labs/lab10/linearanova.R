d3a3372e858c0318c08223a0d3b384e7072b8fa4
jyoung67 <36745757+jyoung67@users.noreply.github.com>
2020-04-08 22:19:14 -0400

    Completed 1B

--- labs/lab10/linearanova.R
+++ labs/lab10/linearanova.R
@@ -1,4 +1,4 @@
-onewayanova <- function(threshold=0.05)
+linearanova <- function(threshold=0.05)
 {
   threshold <- 0.05
   myT<-read.table("/Users/young/Documents/GitHub/advstatistics-labs/labs/lab10/nc101_scaff_dataCounts.txt",sep="\t",header=TRUE,row.names=1)
@@ -14,17 +14,17 @@ onewayanova <- function(threshold=0.05)
     colSum = sum(myT[,i])
     myMat[,i] =myTNorm[,i]/colSum
   }
-   for ( i in 1:numRows)
+  for ( i in 1:numRows)
   {
     D2 <- myMat[i, 1:3]; W12 <- myMat[i, 4:6]; W18 <- myMat[i, 7:11]
     data <- c(D2, W12, W18)
-    period <- c(rep("day 2", length(D2)), rep("week 12", length(W12)), rep("week 18", length(W18)))
-    myLm <- lm(data ~ factor(period), x=TRUE)
+    period <- c(rep(2, length(D2)), rep(86, length(W12)), rep(128, length(W18)))
+    myLm <- lm(data ~ period, x = TRUE)
     rawPvalues[i] <- anova(myLm)$"Pr(>F)"[1]
     
-   }
+  }
   cat("# of significant genes at BH FDR-corrected 0.05 threshold:", sum(p.adjust(rawPvalues, method =  "BH") < threshold))
   
-  hist(rawPvalues, breaks = 50, main = "Raw P- values (one-way ANOVA)")
+  hist(rawPvalues, breaks = 50, main = "Raw P- values (linear regression ANOVA)")
   return (rawPvalues)
 }
