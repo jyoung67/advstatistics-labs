a386ff8bc2747f2c58ca9db5d4020fc3e4cbb9b7
jyoung67 <36745757+jyoung67@users.noreply.github.com>
2020-04-09 23:03:08 -0400

    Intermediate Updates

--- labs/lab10/linearanovamostsig.R
+++ labs/lab10/linearanovamostsig.R
@@ -1,9 +1,25 @@
 linearanovamostsig <- function()
 {
   source('~/GitHub/advstatistics-labs/labs/lab10/linearanova.R')
-  period <- c(rep(2, 3), rep(86, 3), rep(128, 5))
+  library("ggplot2")
+  
+  periods <- c(rep(2, 3), rep(86, 3), rep(128, 5))
   ls <- linearanova()
   minIndex <- order(ls$pValues)[1]
-  cat("Minimum index:", minIndex)
-  boxplot(ls$sourceData[minIndex,]~period, log = "y", names=c("2 days","86 days","128 days"), xlab="", ylab="relative abundance", main="Most Significant Gene Values for Reduced Model")
+  myData <- ls$sourceData[minIndex,]
+  myLm <- lm( myData ~ periods, x=TRUE)
+  dat <- data.frame(
+    x = periods,
+    y = myData 
+  )
+  cat("Minimum pvalue index:",  minIndex)
+  coefs <- coef(lm(y ~ x, data = dat))
+  bp <- ggplot(dat, aes(x, y, group = x)) + geom_boxplot()
+  bp + geom_abline(intercept = coefs[1], slope = coefs[2]) + 
+  scale_x_continuous(breaks=c(2,86,128), labels=c(2, 86, 128), limits=c(0,150)) + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+  panel.background = element_blank(), axis.line = element_line(colour = "black")) + 
+  ggtitle("Most Sig Gene Values for Reduced Model") +
+  xlab("Days") + 
+  ylab("relative abundance")
 }
