3139c0e87aef5792f88879002c85eceb34544e03
jyoung67 <36745757+jyoung67@users.noreply.github.com>
2020-04-24 17:53:05 -0400

    Update getLmeStats.R

--- labs/Lab12/getLmeStats.R
+++ labs/Lab12/getLmeStats.R
@@ -1,13 +1,16 @@
 getLmeStats <- function()
 {
+  library("nlme")
   source('~/GitHub/advstatistics-labs/labs/Lab12/getIndexesForNominals.R')
+  threshold <- 0.10
   
-  # Calculates intraclass correlation coefficient
+  # Calculates intraclass correlation coefficient and p-value
   getStatValues <- function(lmeModel)
   {
     varcor <- sapply(VarCorr(lmeModel)[,2], as.double)
     icc <- varcor[1]^2/(varcor[1]^2+ varcor[2]^2)
     pvalue <- unclass(summary(lmeModel))$tTable[2,5]
+    cat("ICC: ", icc, " | ", "p-value: ", pvalue, "\n")
     return (data.frame(icc=icc, pvalue=pvalue))
   }
   
@@ -17,30 +20,29 @@ getLmeStats <- function()
   myT <- rawData[rawData$time == "POST",]
   cages <- as.vector(myT$cage)
   cageIndexes <- getIndexesForNominals(cages)
-  genotypes <- genotype=as.vector(myT$genotype)
+  genotypes <- as.vector(myT$genotype)
   
-  TenericutesDF <- data.frame(bugData=as.vector(myT$Tenericutes), cages=cages, genotypes=genotypes)
-  VerrucomicrobiaDF <- data.frame(bugData=as.vector(myT$Verrucomicrobia), cages=cages, genotypes=genotypes)
-  BacteroidetesDF <- data.frame(bugData=as.vector(myT$Bacteroidetes), cages=cages, genotypes=genotypes)
-  ActinobacteriaDF <- data.frame(bugData=as.vector(myT$Actinobacteria), cages=cages, genotypes=genotypes)
-  FirmicutesDF <- data.frame(bugData=as.vector(myT$Firmicutes), cages=cages, genotypes=genotypes)
-  ProteobacteriaDF <- data.frame(bugData=as.vector(myT$Proteobacteria), cages=cages, genotypes=genotypes)
+  TenericutesDF <- data.frame(bugData=as.vector(myT$Tenericutes), cage=cages, genotype=genotypes)
+  VerrucomicrobiaDF <- data.frame(bugData=as.vector(myT$Verrucomicrobia), cage=cages, genotype=genotypes)
+  BacteroidetesDF <- data.frame(bugData=as.vector(myT$Bacteroidetes), cage=cages, genotype=genotypes)
+  ActinobacteriaDF <- data.frame(bugData=as.vector(myT$Actinobacteria), cage=cages, genotype=genotypes)
+  FirmicutesDF <- data.frame(bugData=as.vector(myT$Firmicutes), cage=cages, genotype=genotypes)
+  ProteobacteriaDF <- data.frame(bugData=as.vector(myT$Proteobacteria), cage=cages, genotype=genotypes)
   
-  Tenericutes.mixed <- lme( bugData ~ genotypes, method= "REML", random = ~1 | cages, data = TenericutesDF)
-  Verrucomicrobia.mixed <- lme( bugData ~ genotypes, method= "REML", random = ~1 | cages, data = Verrucomicrobia)
-  Bacteroidetes.mixed <- lme( bugData ~ genotypes, method= "REML", random = ~1 | cages, data = Bacteroidetes)
-  Actinobacteria.mixed <- lme( bugData ~ genotypes, method= "REML", random = ~1 | cages, data = Actinobacteria)
-  Firmicutes.mixed <- lme( bugData ~ genotypes, method= "REML", random = ~1 | cages, data = Firmicutes)
-  Proteobacteria.mixed <- lme( bugData ~ genotypes, method= "REML", random = ~1 | cages, data = Proteobacteria)
-  pValuesMixed[index] <- anova(M.mixed)$"p-value"[2]
+  Tenericutes.mixed <- lme( bugData ~ genotype, method= "REML", random = ~1 | cage, data = TenericutesDF)
+  Verrucomicrobia.mixed <- lme( bugData ~ genotype, method= "REML", random = ~1 | cage, data = VerrucomicrobiaDF)
+  Bacteroidetes.mixed <- lme( bugData ~ genotype, method= "REML", random = ~1 | cage, data = BacteroidetesDF)
+  Actinobacteria.mixed <- lme( bugData ~ genotype, method= "REML", random = ~1 | cage, data = ActinobacteriaDF)
+  Firmicutes.mixed <- lme( bugData ~ genotype, method= "REML", random = ~1 | cage, data = FirmicutesDF)
+  Proteobacteria.mixed <- lme( bugData ~ genotype, method= "REML", random = ~1 | cage, data = ProteobacteriaDF)
   
-  
-  
-  
- 
- 
-  
- 
+  TResult <- getStatValues(Tenericutes.mixed)
+  VResult <- getStatValues(Verrucomicrobia.mixed)
+  BResult <- getStatValues(Bacteroidetes.mixed)
+  AResult <- getStatValues(Actinobacteria.mixed)
+  FResult <- getStatValues(Firmicutes.mixed)
+  PResult <- getStatValues(Proteobacteria.mixed)
   
   
+  cat("Number of significant p-values at 0.10 threshold: ", sum(p.adjust(c(TResult, VResult, BResult, AResult, FResult, PResult), method =  "BH") < threshold))
 }
