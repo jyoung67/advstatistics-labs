1f8340cb96d4f378e228cd8150cedf2fe40c094a
jyoung67 <36745757+jyoung67@users.noreply.github.com>
2020-04-26 10:52:53 -0400

    Additional Updates

--- labs/Lab12/generateBoxPlots.R
+++ labs/Lab12/generateBoxPlots.R
@@ -1,14 +1,12 @@
 generateBoxPlots <- function()
 {
-  source('~/GitHub/advstatistics-labs/labs/Lab12/getIndexesForNominals.R')
-  
   # Read/transform data
   inFileName <- "http://afodor.github.io/classes/stats2015/prePostPhylum.txt"
   rawData <-read.table(inFileName,header=TRUE,sep="\t")
   myT <- rawData[rawData$time == "POST",]
   cages <- as.vector(myT$cage)
-  cageIndexes <- getIndexesForNominals(cages)
   
+  # Create data frame for explanatory variables
   bugData <- data.frame(Tenericutes=as.vector(myT$Tenericutes), 
                         Verrucomicrobia=as.vector(myT$Verrucomicrobia), 
                         Bacteroidetes=as.vector(myT$Bacteroidetes),
@@ -16,19 +14,23 @@ generateBoxPlots <- function()
                         Firmicutes=as.vector(myT$Firmicutes), 
                         Proteobacteria=as.vector(myT$Proteobacteria))
   
-  nominalData <- data.frame(cage=as.vector(myT$cage), 
-                            genotype=as.vector(myT$genotype), 
-                            cageIndexes=cageIndexes$indexVector)
+  # Trim cage label text
+  trimmedUniqueLabelVector <- sapply(myT$cage, gsub, pattern = "Cage", replacement ="C")
+  
+  # Create data frame for nominal variables
+  nominalData <- data.frame(cage=as.vector(trimmedUniqueLabelVector),genotype=as.vector(myT$genotype))
+  
+  # Set plot parameters
   op <- par(mfrow = c(3,2))
-  xlabText <- "cage ref #"
-  ylabText <- "abundance"
-  
+  ylabText <- "relative abundance"
   columnNames <- colnames(bugData)
-  xComponent <- nominalData$cageIndexes
+  xComponent <- nominalData$cage
   
+  # Generate box plots
   for (i in 1:length(columnNames))
   {
-    boxplot(bugData[[i]]~xComponent,xlab = "", ylab = ylabText, main = paste(columnNames[i], "by Cage"), names=cageIndexes$uniqueLabelVector, las = 2)
+    boxplot(bugData[[i]]~xComponent,xlab = "", ylab = ylabText, main = paste(columnNames[i], "by Cage"), las = 2)
+    abline(h=mean((bugData[[i]])), col="blue", lty=3)
     stripchart(bugData[[i]]~xComponent, vertical=TRUE, pch=21,cex=0.8,col="red", add=TRUE)
   }
   par(op)
